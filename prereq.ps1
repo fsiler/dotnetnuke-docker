3b1f388fabb1b6c0498ab9a892967613574112b0
Frank Siler <me@franksiler.com>
2019-03-24 08:37:17 +0000

    adding choco confirm by default

--- prereq.ps1
+++ prereq.ps1
@@ -1,13 +1,12 @@
 iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
-choco install dotnet4.5.2 -y
+choco feature enable -n=allowGlobalConfirmation
+
+choco install dotnet4.5.2
 Add-WindowsFeature Web-Asp-Net45 
-#==================
-# Global Flags ====
+
 Install-PackageProvider -Name NuGet -force
-#Set-PSDebug -Trace 2
 $ErrorActionPreference = 'Stop'
-# Main ============
-#Install-PackageProvider -Name NuGet -Force
+
 Set-PSRepository -Name 'PSGallery' -InstallationPolicy Trusted
 Install-Module xWebAdministration -Force
 Install-Module cNtfsAccessControl -Force
